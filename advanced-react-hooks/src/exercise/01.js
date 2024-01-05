e61208c089ba127b81ba82dd52d87053c5481fe5
Ryan Rasmussen <ryanrasmussen05@gmail.com>
2024-01-05 15:34:47 -0600

    Add files via upload

---
+++ advanced-react-hooks/src/exercise/01.js
@@ -0,0 +1,25 @@
+// useReducer: simple Counter
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from 'react'
+
+function countReducer(state, action) {
+  if (action.type === 'INCREMENT') {
+    return { ...state, count: state.count + action.step };
+  }
+  return { ...state };
+}
+
+function Counter({initialCount = 0, step = 1}) {
+  const [state, dispatch] = React.useReducer(countReducer, { count: initialCount });
+  const { count } = state;
+
+  const increment = () => dispatch({ type: 'INCREMENT', step });
+  return <button onClick={increment}>{count}</button>
+}
+
+function App() {
+  return <Counter />
+}
+
+export default App
